859ba640dfd249be3cca1b002e54ef0d0d38f515
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 13:55:00 -0700

    Reverses a string

--- aux_str3.c
+++ aux_str3.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string - Function reverses a string.
+ * rev_string - reverses a string.
  * @s: input string.
  * Return: no return.
  */
@@ -13,18 +13,3 @@ void rev_string(char *s)
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
-			break;
-		count++;
-	}
-	str = s;
-
-	for (i = 0; i < (count - 1); i++)
-	{
-		for (j = i + 1; j > 0; j--)
-		{
-			temp = *(str + j);
-			*(str + j) = *(str + (j - 1));
-			*(str + (j - 1)) = temp;
-		}
-	}
-}
