88bac33ea4b0aee5220ed254b8e4304f26db7181
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 13:15:53 -0700

    Exits the shell

--- exit_shell.c
+++ exit_shell.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * exit_shell - This function exits the shell
- *
- * @datash: data relevant (status and args)
- * Return: 0 on success.
+ * exit_shell - exit the shell
+ * @datash: relevant data
+ * Return: 0 on success
  */
 int exit_shell(data_shell *datash)
 {
@@ -14,18 +13,3 @@ int exit_shell(data_shell *datash)
 	int big_number;
 
 	if (datash->args[1] != NULL)
-	{
-		ustatus = _atoi(datash->args[1]);
-		is_digit = _isdigit(datash->args[1]);
-		str_len = _strlen(datash->args[1]);
-		big_number = ustatus > (unsigned int)INT_MAX;
-		if (!is_digit || str_len > 10 || big_number)
-		{
-			get_error(datash, 2);
-			datash->status = 2;
-			return (1);
-		}
-		datash->status = (ustatus % 256);
-	}
-	return (0);
-}
