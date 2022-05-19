36af7140cccb9cd00650e09f3d9441d825910310
princeardayfio <princeardayfio@yahoo.com>
2022-05-19 20:38:28 +0000

    environment files

--- env1.c
+++ env1.c
@@ -33,3 +33,42 @@ int cmp_env_name(const char *nenv, const char *name)
 char *_getenv(const char *name, char **_environ)
 {
 	char *ptr_env;
+	int i, mov;
+
+	ptr_env = NULL;
+	mov = 0;
+
+	for (i = 0; _environ[i]; i++)
+	{
+		mov = cmp_env_name(_environ[i], name);
+		if (mov)
+		{
+			ptr_env = _environ[i];
+			break;
+		}
+	}
+
+	return (ptr_env + mov);
+}
+
+/**
+ * _env - prints the environment variables
+ * @datash: data relevant.
+ * Return: 1 on success.
+ */
+int _env(data_shell *datash)
+{
+	int i, j;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		for (j = 0; datash->_environ[i][j]; j++)
+			;
+		write(STDOUT_FILENO, datash->_environ[i], j);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+
+	datash->status = 0;
+
+	return (1);
+}
