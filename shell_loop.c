9f71a08eb6ba90dcd91d85f856649ee6cb9b3543
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 14:48:20 -0700

    Update

--- shell_loop.c
+++ shell_loop.c
@@ -2,7 +2,6 @@
 
 /**
  * without_comment - deletes comments from the input
- *
  * @in: input string
  * Return: input without comments
  */
@@ -36,42 +35,6 @@ char *without_comment(char *in)
 }
 
 /**
- * shell_loop - Loop of shell
- * @datash: data relevant (av, input, args)
- *
- * Return: no return.
- */
-void shell_loop(data_shell *datash)
-{
-	int loop, i_eof;
-	char *input;
-
-	loop = 1;
-	while (loop == 1)
-	{
-		write(STDIN_FILENO, "^-^ ", 4);
-		input = read_line(&i_eof);
-		if (i_eof != -1)
-		{
-			input = without_comment(input);
-			if (input == NULL)
-				continue;
-
-			if (check_syntax_error(datash, input) == 1)
-			{
-				datash->status = 2;
-				free(input);
-				continue;
-			}
-			input = rep_var(input, datash);
-			loop = split_commands(datash, input);
-			datash->counter += 1;
-			free(input);
-		}
-		else
-		{
-			loop = 0;
-			free(input);
-		}
-	}
-}
+ * shell_loop - loop of the shell
+ * @datash: relevant data
+ * Return: no return
