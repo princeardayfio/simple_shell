3d454c25662cc94cfe6508dd1498c2c7dfcb62a6
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 13:12:52 -0700

    Builtin that performs the command

--- get_builtin.c
+++ get_builtin.c
@@ -12,3 +12,17 @@ int (*get_builtin(char *cmd))(data_shell *)
 		{"exit", exit_shell},
 		{"setenv", _setenv},
 		{"unsetenv", _unsetenv},
+		{"cd", cd_shell},
+		{"help", get_help},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
