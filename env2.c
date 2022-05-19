594e7a3591a05cbbf5f0f779ca7af1f4ee2ac74a
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 13:28:46 -0700

    Copies info to create a new env or alias

--- env2.c
+++ env2.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * copy_info - copies info to create
- * a new env or alias
+ * copy_info - copeis info tocreate a new env or alias
  * @name: name (env or alias)
- * @value: value (env or alias)
- *
- * Return: new env or alias.
+ * @value: value
+ * Return: new env or alias
  */
 char *copy_info(char *name, char *value)
 {
@@ -26,12 +24,11 @@ char *copy_info(char *name, char *value)
 }
 
 /**
- * set_env - sets an environment variable
- *
- * @name: name of the environment variable
- * @value: value of the environment variable
- * @datash: data structure (environ)
- * Return: no return
+ * set_env -sets an environment variable
+ * @name: nameof the variable
+ * @value: value of the variable
+ * @datash: data structure for the shell
+ * Return: nothing
  */
 void set_env(char *name, char *value, data_shell *datash)
 {
@@ -58,72 +55,6 @@ void set_env(char *name, char *value, data_shell *datash)
 }
 
 /**
- * _setenv - compares env variables names
- * with the name passed.
- * @datash: data relevant (env name and env value)
- *
- * Return: 1 on success.
- */
-int _setenv(data_shell *datash)
-{
-
-	if (datash->args[1] == NULL || datash->args[2] == NULL)
-	{
-		get_error(datash, -1);
-		return (1);
-	}
-
-	set_env(datash->args[1], datash->args[2], datash);
-
-	return (1);
-}
-
-/**
- * _unsetenv - deletes a environment variable
- *
- * @datash: data relevant (env name)
- *
- * Return: 1 on success.
- */
-int _unsetenv(data_shell *datash)
-{
-	char **realloc_environ;
-	char *var_env, *name_env;
-	int i, j, k;
-
-	if (datash->args[1] == NULL)
-	{
-		get_error(datash, -1);
-		return (1);
-	}
-	k = -1;
-	for (i = 0; datash->_environ[i]; i++)
-	{
-		var_env = _strdup(datash->_environ[i]);
-		name_env = _strtok(var_env, "=");
-		if (_strcmp(name_env, datash->args[1]) == 0)
-		{
-			k = i;
-		}
-		free(var_env);
-	}
-	if (k == -1)
-	{
-		get_error(datash, -1);
-		return (1);
-	}
-	realloc_environ = malloc(sizeof(char *) * (i));
-	for (i = j = 0; datash->_environ[i]; i++)
-	{
-		if (i != k)
-		{
-			realloc_environ[j] = datash->_environ[i];
-			j++;
-		}
-	}
-	realloc_environ[j] = NULL;
-	free(datash->_environ[k]);
-	free(datash->_environ);
-	datash->_environ = realloc_environ;
-	return (1);
-}
+ * _setenv - compares env variable names
+ * @datash: relevant data
+ * Return: 1 on success
