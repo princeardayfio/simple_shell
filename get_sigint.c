60ced892b4087411414d25fe4301c5a7cf9df036
princeardayfio <princeardayfio@yahoo.com>
2022-05-19 19:54:55 +0000

    feature update

--- get_sigint.c
+++ get_sigint.c
@@ -1,11 +1,6 @@
 #include "main.h"
 
 /**
- * get_sigint - Handle the crtl + c call in prompt
- * @sig: Signal handler
+ * get_sigint - handle the ctrl+c call in prompt
+ * @sig: signal number
  */
-void get_sigint(int sig)
-{
-	(void)sig;
-	write(STDOUT_FILENO, "\n^-^ ", 5);
-}
