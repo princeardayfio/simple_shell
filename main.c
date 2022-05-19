2c2ca348b604253942af650aeb9503ee96329bed
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 12:53:13 -0700

    Frees the data structure

--- main.c
+++ main.c
@@ -46,3 +46,24 @@ void set_data(data_shell *datash, char **av)
 
 	datash->_environ[i] = NULL;
 	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ * @ac: argument count
+ * @av: argument Vector
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
