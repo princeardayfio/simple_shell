27b7e252034c142901bb80fbc2c80045db49969f
princeardayfio <princeardayfio@yahoo.com>
2022-05-19 20:57:33 +0000

    reverses a string

--- aux_str3.c
+++ aux_str3.c
@@ -13,3 +13,18 @@ void rev_string(char *s)
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
+}
