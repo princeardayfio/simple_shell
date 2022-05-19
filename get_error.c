e1a91ef02d2507e9bc4d43bbe0bfbe0a88604f27
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 13:09:55 -0700

    Calls the error according to the builtin, syntax or permission

--- get_error.c
+++ get_error.c
@@ -19,3 +19,22 @@ int get_error(data_shell *datash, int eval)
 		error = error_path_126(datash);
 		break;
 	case 127:
+			error = error_not_found(datash);
+		break;
+	case 2:
+		if (_strcmp("exit", datash->args[0]) == 0)
+			error = error_exit_shell(datash);
+		else if (_strcmp("cd", datash->args[0]) == 0)
+			error = error_get_cd(datash);
+		break;
+	}
+
+	if (error)
+	{
+		write(STDERR_FILENO, error, _strlen(error));
+		free(error);
+	}
+
+	datash->status = eval;
+	return (eval);
+}
