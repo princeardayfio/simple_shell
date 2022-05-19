a0f13b2b72eef6f2fecfb7385c15c3b844a331ff
princeardayfio <princeardayfio@yahoo.com>
2022-05-19 20:19:43 +0000

    feature update

--- exit_shell.c
+++ exit_shell.c
@@ -1,4 +1,4 @@
-#include "main.h"
+}#include "main.h"
 
 /**
  * exit_shell - exit the shell
@@ -13,3 +13,18 @@ int exit_shell(data_shell *datash)
 	int big_number;
 
 	if (datash->args[1] != NULL)
+	{
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
+		datash->status = (ustatus % 256);
+	}
+	return (0);
+}
