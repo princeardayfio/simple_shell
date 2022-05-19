4c49f3bb6e6a021382f05c4d6e762bbe1923080e
theclubfoot <papakwesiadams@icloud.com>
2022-05-19 13:48:48 -0700

    Changes current directory

--- cd_shell.c
+++ cd_shell.c
@@ -17,28 +17,3 @@ int cd_shell(data_shell *datash)
 	{
 		ishome = _strcmp("$HOME", dir);
 		ishome2 = _strcmp("~", dir);
-		isddash = _strcmp("--", dir);
-	}
-
-	if (dir == NULL || !ishome || !ishome2 || !isddash)
-	{
-		cd_to_home(datash);
-		return (1);
-	}
-
-	if (_strcmp("-", dir) == 0)
-	{
-		cd_previous(datash);
-		return (1);
-	}
-
-	if (_strcmp(".", dir) == 0 || _strcmp("..", dir) == 0)
-	{
-		cd_dot(datash);
-		return (1);
-	}
-
-	cd_to(datash);
-
-	return (1);
-}
